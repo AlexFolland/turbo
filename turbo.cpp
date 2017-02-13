6e4ba6a9e84050aa1f855afe1fc4f3e935a3e444
AlexFolland <lexlexlex@gmail.com>
2017-02-13 00:06:17 -0800

    merge turboalternate.cpp with turbo.cpp increase int precision to 64-bit for tick multiplication

--- turbo.cpp
+++ turbo.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-//#include <chrono>
-//#include <thread>
+#include <chrono>
+#include <thread>
 #include <Windows.h>
 using namespace std;
 
@@ -9,37 +9,46 @@ int main()
 	// declarations and definitions
 
 	LARGE_INTEGER ticksPerSecond;   // timer frequency, defined by QueryPerformanceFrequency()
+	LARGE_INTEGER recheckedFrequency;   // timer frequency, checked again each loop
 	LARGE_INTEGER tick;             // tick to get on every loop for timing
-	int tickFrames;                 // the above in WA frame lengths
+	unsigned __int64 tickFrames;                 // the above in WA frame lengths
 
 	char kbState[256];              // array for GetKeyboardState() to dump into
 	HKL kbLayout;
 
 	bool laltHeldLast = false;      // whether the space-mashing function key was held in the last loop
 	LARGE_INTEGER laltStart;        // point at which the space-mashing function key began being held
-	int laltStartFrames;            // laltStart in WA frame lengths
+	unsigned __int64 laltStartFrames;            // laltStart in WA frame lengths
 	LARGE_INTEGER laltTick;        	// point at which the space-mashing function key began being held
-	int laltTickFrames;             // laltTick in WA frame lengths
-	int laltHeldFrames;				// number of frames the space-mashing function key was held so far
-	int lastLaltHeldFrames;			//number of frames the space-mashing function key was held as of the previous loop
+	unsigned __int64 laltTickFrames;             // laltTick in WA frame lengths
+	unsigned __int64 laltHeldFrames;				// number of frames the space-mashing function key was held so far
+	unsigned __int64 lastLaltHeldFrames;			//number of frames the space-mashing function key was held as of the previous loop
 
 	bool leftHeldLast = false;      // whether the left skip-walk function key was held in the last loop
 	bool leftDown = false;          // ..
 	LARGE_INTEGER leftStart;        // point at which the left skip-walk function key began being held
-	int leftStartFrames;            // leftStart in WA frame lengths
+	unsigned __int64 leftStartFrames;            // leftStart in WA frame lengths
 	LARGE_INTEGER leftTick;         // for comparison between timer query points
-	int leftTickFrames;             // leftTick in WA frame lengths
-	int leftHeldFrames;             // length of time the left skip-walk function key was held for
+	unsigned __int64 leftTickFrames;             // leftTick in WA frame lengths
+	unsigned __int64 leftHeldFrames;             // length of time the left skip-walk function key was held for
 
 	bool rightHeldLast = false;     // whether the right skip-walk function key was held in the last loop
 	bool rightDown = false;         // ..
 	LARGE_INTEGER rightStart;       // point at which the left skip-walk function key began being held
-	int rightStartFrames;           // rightStart in WA frame lengths
+	unsigned __int64 rightStartFrames;           // rightStart in WA frame lengths
 	LARGE_INTEGER rightTick;        // for comparison between timer query points
-	int rightTickFrames;            // rightTick in WA frame lengths
-	int rightHeldFrames;            // length of time the left skip-walk function key was held for
+	unsigned __int64 rightTickFrames;            // rightTick in WA frame lengths
+	unsigned __int64 rightHeldFrames;            // length of time the left skip-walk function key was held for
 
-	if(!QueryPerformanceFrequency(&ticksPerSecond))                                 // QueryPerformanceFrequency() execution and error check
+	bool alternateHeldLast = false; // whether the arrow-key-alternating function key was held in the last loop
+	bool alternateDown = false;     // ..
+	LARGE_INTEGER alternateStart;   // point at which the arrow-key-alternating function key began being held
+	unsigned __int64 alternateStartFrames;       // alternateStart in WA frame lengths
+	LARGE_INTEGER alternateTick;    // for comparison between timer query points
+	unsigned __int64 alternateTickFrames;        // alternateTick in WA frame lengths
+	unsigned __int64 alternateHeldFrames;        // length of time the arrow-key-alternating function key was held for
+
+	if(!QueryPerformanceFrequency(&ticksPerSecond))								// QueryPerformanceFrequency() execution and error check
 	{
 		cout << "Error: QueryPerformanceFrequency() returned false.  This system has no" << endl
 			 << "high-resolution timer.  Quitting." << endl;
@@ -59,6 +68,9 @@ int main()
 		 << " numpad 6      | skip-walk right"                                                << endl
 		 << " qwerty X      |"                                                                << endl
 		 << "---------------|---------------------------------------------------------------" << endl
+		 << " numpad 5      | alternate left and right arrow keys"                            << endl
+		 << " qwerty C      |"                                                                << endl
+		 << "---------------|---------------------------------------------------------------" << endl
 		 << " ctrl+break    | quit"                                                           << endl
 		 << "---------------|---------------------------------------------------------------" << endl
 		 <<                                                                                      endl
@@ -69,8 +81,14 @@ int main()
 
 	do
 	{
-		//std::this_thread::sleep_for(std::chrono::microseconds(1));
-		Sleep(1);                                                             // wait time between input-checking loops (in milliseconds)
+		std::this_thread::sleep_for(std::chrono::microseconds(1000));
+		//Sleep(1);																	// wait time between input-checking loops (in milliseconds)
+		QueryPerformanceFrequency(&recheckedFrequency);
+		if(ticksPerSecond.QuadPart != recheckedFrequency.QuadPart)
+		{
+			cout << "QueryPerformanceFrequency returned a different frequency." << endl;
+			ticksPerSecond = recheckedFrequency;
+		}
 		QueryPerformanceCounter(&tick);                                             // generic tick
 		tickFrames = tick.QuadPart*50/ticksPerSecond.QuadPart;                      // convert to WA frames
 
@@ -102,7 +120,7 @@ int main()
 			// Lex 2007.02.27: i'm keeping the empty {}s for alignment, but removing the useless "0 | "s.
 			// Lex 2012.03.11: removed empty {}s
 
-		if(/*kbState[VK_LMENU]<0 || */kbState[VK_NUMPAD0]<0)
+		if(kbState[VK_LMENU]<0 || kbState[VK_NUMPAD0]<0)
 		{
 			if (!laltHeldLast)
 			{
@@ -201,6 +219,45 @@ int main()
 				cout << "right released (during clean-up)" << endl;
 			}
 			rightHeldLast = false;
+		}
+
+		/////////////// numpad 5 - alternate left and right arrow keys ///////////////
+
+		if(kbState[VK_NUMPAD5]<0 || kbState[MapVirtualKeyEx(0x2E,3,kbLayout)]<0)
+		{
+			if(!alternateHeldLast)
+			{
+				cout << "5 pressed" << endl;
+				QueryPerformanceCounter(&alternateStart);
+				alternateStartFrames = alternateStart.QuadPart*50/ticksPerSecond.QuadPart;    // convert to WA frame lengths
+				keybd_event(VK_LEFT,0,0,0);
+				cout << "left pressed" << endl;
+			}
+			alternateHeldFrames = tickFrames - alternateStartFrames;                      // get however much time was spent looping, converted into WA frame lengths (0.02 seconds each)
+			bool targetAlternateState = (alternateHeldFrames % 2 != 1);                      // every other frame
+			if(alternateDown != targetAlternateState)     // VP 2007.02.27: synchronize desired state with actual state
+			{
+				keybd_event(VK_RIGHT, 0, targetAlternateState ? 0 : KEYEVENTF_KEYUP, 0);
+				cout << (targetAlternateState ? "left pressed" : "right pressed") << endl;
+				alternateDown = targetAlternateState;
+			}
+			else keybd_event(VK_LEFT, 0, targetAlternateState ? KEYEVENTF_KEYUP : 0, 0);
+			alternateHeldLast = true;
+		}
+		else if(alternateHeldLast)                                                       // clean up if user released numpad 4
+		{
+			cout << "5 released" << endl;
+			if(kbState[VK_LEFT]<0)
+			{
+				keybd_event(VK_LEFT,0,KEYEVENTF_KEYUP,0);
+				cout << "left released (during clean-up)" << endl;
+			}
+			if(kbState[VK_RIGHT]<0)
+			{
+				keybd_event(VK_RIGHT,0,KEYEVENTF_KEYUP,0);
+				cout << "right released (during clean-up)" << endl;
+			}
+			alternateHeldLast = false;
 		}
 
 		/////////////// ctrl+break - quit ///////////////

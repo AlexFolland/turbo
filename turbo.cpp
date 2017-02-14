c70e1987c43af183369c920d603082b6a44086ff
AlexFolland <lexlexlex@gmail.com>
2017-02-14 07:04:53 -0800

    weird shit but kinda works; FIX THIS

--- turbo.cpp
+++ turbo.cpp
@@ -5,6 +5,43 @@
 #include <Windows.h>
 using namespace std;
 
+// declarations and definitions
+
+float targetFPS = 50;											// target frames per second
+float targetGranularity = 10;									// target number of times to poll per frame at target frame rate
+unsigned long long sleepLength = (unsigned long long)(1000000.0f/(targetGranularity*targetFPS));
+
+LARGE_INTEGER ticksPerSecond;												// timer frequency, defined by QueryPerformanceFrequency()
+LARGE_INTEGER tick;															// tick to get on every loop for timing
+float tickFrames;												// the above in target frame lengths
+float lastTickFrames = 0;										// for time comparison with previous loop
+
+bool laltHeldLast = false;													// whether the space-mashing function key was held in the last loop
+float laltStartFrames = 0;										// laltStart in target frame lengths
+float laltTickFrames = 0;										// laltTick in target frame lengths
+float laltHeldFrames = 0;										// number of frames the space-mashing function key was held so far
+float lastLaltHeldFrames = 0;									// number of frames the space-mashing function key was held as of the previous loop
+
+bool leftHeldLast = false;													// whether the left skip-walk function key was held in the last loop
+float leftStartFrames;											// leftStart in target frame lengths
+float leftTickFrames;											// leftTick in target frame lengths
+float leftHeldFrames;											// length of time the left skip-walk function key was held for
+
+bool rightHeldLast = false;													// whether the right skip-walk function key was held in the last loop
+float rightStartFrames;										// rightStart in target frame lengths
+float rightTickFrames;											// rightTick in target frame lengths
+float rightHeldFrames;											// length of time the left skip-walk function key was held for
+
+bool alternateHeldLast = false;												// whether the arrow-key-alternating function key was held in the last loop
+bool alternateDown = false;													// ..
+float alternateStartFrames;									// alternateStart in target frame lengths
+float alternateTickFrames;										// alternateTick in target frame lengths
+float alternateHeldFrames;										// length of time the arrow-key-alternating function key was held for
+float lastAlternateHeldFrames;									// number of frames the arrow-key-alternating function key was held as of the previous loop
+bool goingRight = false;													// whether currently going right
+
+HKL kbLayout = GetKeyboardLayout(0);
+
 namespace turbo
 {
 	void SendKeyboardInput(WORD code, bool isKeyUp = false, bool isScanCode = false)
@@ -23,7 +60,6 @@ namespace turbo
 	{
 		if(isScanCode)
 		{
-			HKL kbLayout = GetKeyboardLayout(0);
 			code = MapVirtualKeyEx(code,MAPVK_VSC_TO_VK_EX,kbLayout);
 		}
 		return GetKeyState(code) & 0x8000;
@@ -34,47 +70,6 @@ using namespace turbo;
 
 int main()
 {
-	// declarations and definitions
-
-	unsigned int targetFPS = 50;												// target frames per second
-	unsigned int targetGranularity = 2;											// target number of times to poll per frame at target frame rate
-	LARGE_INTEGER ticksPerSecond;												// timer frequency, defined by QueryPerformanceFrequency()
-	LARGE_INTEGER tick;															// tick to get on every loop for timing
-	unsigned long long tickFrames;												// the above in target frame lengths
-
-	bool laltHeldLast = false;													// whether the space-mashing function key was held in the last loop
-	LARGE_INTEGER laltStart;													// point at which the space-mashing function key began being held
-	unsigned long long laltStartFrames = 0;										// laltStart in target frame lengths
-	LARGE_INTEGER laltTick;														// point at which the space-mashing function key began being held
-	unsigned long long laltTickFrames = 0;										// laltTick in target frame lengths
-	unsigned long long laltHeldFrames = 0;										// number of frames the space-mashing function key was held so far
-	unsigned long long lastLaltHeldFrames = 0;									// number of frames the space-mashing function key was held as of the previous loop
-
-	bool leftHeldLast = false;													// whether the left skip-walk function key was held in the last loop
-	bool leftDown = false;														// ..
-	LARGE_INTEGER leftStart;													// point at which the left skip-walk function key began being held
-	unsigned long long leftStartFrames;											// leftStart in target frame lengths
-	LARGE_INTEGER leftTick;														// for comparison between timer query points
-	unsigned long long leftTickFrames;											// leftTick in target frame lengths
-	unsigned long long leftHeldFrames;											// length of time the left skip-walk function key was held for
-
-	bool rightHeldLast = false;													// whether the right skip-walk function key was held in the last loop
-	bool rightDown = false;														// ..
-	LARGE_INTEGER rightStart;													// point at which the left skip-walk function key began being held
-	unsigned long long rightStartFrames;										// rightStart in target frame lengths
-	LARGE_INTEGER rightTick;													// for comparison between timer query points
-	unsigned long long rightTickFrames;											// rightTick in target frame lengths
-	unsigned long long rightHeldFrames;											// length of time the left skip-walk function key was held for
-
-	bool alternateHeldLast = false;												// whether the arrow-key-alternating function key was held in the last loop
-	bool alternateDown = false;													// ..
-	LARGE_INTEGER alternateStart;												// point at which the arrow-key-alternating function key began being held
-	unsigned long long alternateStartFrames;									// alternateStart in target frame lengths
-	LARGE_INTEGER alternateTick;												// for comparison between timer query points
-	unsigned long long alternateTickFrames;										// alternateTick in target frame lengths
-	unsigned long long alternateHeldFrames;										// length of time the arrow-key-alternating function key was held for
-	unsigned long long lastAlternateHeldFrames;									// number of frames the arrow-key-alternating function key was held as of the previous loop
-	bool goingRight = false;													// whether currently going right
 
 	if(!QueryPerformanceFrequency(&ticksPerSecond))								// QueryPerformanceFrequency() execution and error check
 	{
@@ -82,6 +77,8 @@ int main()
 			 << "high-resolution timer.  Quitting." << endl;
 		return 1;
 	}
+
+	float sleepLengthFrames = (float)sleepLength/1000000.0f*targetFPS/(float)ticksPerSecond.LowPart;
 
 	// user instructions
 
@@ -94,10 +91,10 @@ int main()
 		 << " qwerty Z      |"																	<< endl
 		 << "---------------|---------------------------------------------------------------"	<< endl
 		 << " numpad 6      | skip-walk right (right 9 frames, release 1 frame, repeat)"		<< endl
-		 << " qwerty X      |"																	<< endl
+		 << " qwerty C      |"																	<< endl
 		 << "---------------|---------------------------------------------------------------"	<< endl
 		 << " numpad 5      | alternate left and right arrow keys each frame"					<< endl
-		 << " qwerty C      |"																	<< endl
+		 << " qwerty X      |"																	<< endl
 		 << "---------------|---------------------------------------------------------------"	<< endl
 		 << " ctrl+break    | quit"																<< endl
 		 << "---------------|---------------------------------------------------------------"	<< endl
@@ -109,10 +106,13 @@ int main()
 
 	do
 	{
-		this_thread::sleep_for(chrono::microseconds((unsigned long long)floor(1000000/(targetGranularity*targetFPS))));									// sleep between checks (number of times per frame dictated by fps and granularity)
+		this_thread::sleep_for(chrono::microseconds(sleepLength));							// sleep between checks (number of times per frame dictated by fps and granularity)
 
-		QueryPerformanceCounter(&tick);																													// generic tick
-		tickFrames = (unsigned long long)floor((unsigned long long)tick.LowPart*targetFPS/(unsigned long long)ticksPerSecond.LowPart);					// convert to WA frames
+		QueryPerformanceCounter(&tick);																												// time since system start in CPU cycles
+		lastTickFrames = tickFrames;
+		tickFrames = (float)tick.LowPart*targetFPS/(float)ticksPerSecond.LowPart;														// convert to number of frames at target frame rate
+
+		if (!(tickFrames - (sleepLengthFrames/2) >= lastTickFrames)) continue;
 	
 		// begin input checks
 		
@@ -124,13 +124,14 @@ int main()
 			// Lex 2007.02.27: this would only matter if you relied on a key to be pressed to press itself in a different way
 			// like if you used space as the space-mashing hotkey.
 
+		kbLayout = GetKeyboardLayout(0);
+
 		if(KeyIsPressed(VK_MENU) || KeyIsPressed(VK_NUMPAD0))
 		{
 			if (!laltHeldLast)
 			{
 				cout << "space mashing started" << endl;
-				QueryPerformanceCounter(&laltStart);
-				laltStartFrames = (unsigned long long)floor((unsigned long long)laltStart.LowPart*targetFPS/(unsigned long long)ticksPerSecond.LowPart);// convert to target frame lengths
+				laltStartFrames = tickFrames;												// set start timestamp
 				SendKeyboardInput(VK_SPACE);
 				//cout << "space pressed" << endl;
 			}
@@ -167,21 +168,17 @@ int main()
 			if(!leftHeldLast)
 			{
 				cout << "skip-walking left started" << endl;
-				QueryPerformanceCounter(&leftStart);
-				// VP 2007.02.27: reusing variables is BAD!
-				leftStartFrames = (unsigned long long)floor((unsigned long long)leftStart.LowPart*targetFPS/(unsigned long long)ticksPerSecond.LowPart);			// convert to WA frame lengths
-				SendKeyboardInput(VK_LEFT);
+				leftStartFrames = tickFrames;												// set start timestamp
+				//SendKeyboardInput(VK_LEFT);
 				//cout << "left pressed" << endl;
-				leftDown = true;
 			}
-			// VP 2007.02.27: this addition caused leftHeldFrames to grow with steady progression - it should be an assignment instead
-			leftHeldFrames = tickFrames - leftStartFrames;									// get however much time was spent looping, converted into target frame lengths (0.02 seconds each for WA)
-			bool targetLeftState = (leftHeldFrames % 11 != 10);								// at the 9th frame, let go of left for a frame
-			if(leftDown != targetLeftState)													// VP 2007.02.27: synchronize desired state with actual state
+
+			leftHeldFrames = tickFrames - leftStartFrames;									// get however many frames this functionality's bind has been held
+			bool targetLeftState = ((long)leftHeldFrames % 11 != 10);								// at the 9th frame, let go of left for a frame
+			if(KeyIsPressed(VK_LEFT) != targetLeftState)									// VP 2007.02.27: synchronize desired state with actual state
 			{
 				SendKeyboardInput(VK_LEFT, !targetLeftState);
 				//cout << "left " << (targetLeftState ? "pressed" : "released") << endl;
-				leftDown = targetLeftState;
 			}
 			leftHeldLast = true;
 		}
@@ -199,26 +196,22 @@ int main()
 
 		/////////////// numpad 6 - skip-walk right ///////////////
 
-		if(KeyIsPressed(VK_NUMPAD6) || KeyIsPressed(0x2D,true))
+		if(KeyIsPressed(VK_NUMPAD6) || KeyIsPressed(0x2E,true))
 		{
 			if(!rightHeldLast)
 			{
 				cout << "skip-walking right started" << endl;
-				QueryPerformanceCounter(&rightStart);
-				// VP 2007.02.27: reusing variables is BAD!
-				rightStartFrames = (unsigned long long)floor((unsigned long long)rightStart.LowPart*targetFPS/(unsigned long long)ticksPerSecond.LowPart);			// convert to WA frame lengths
-				SendKeyboardInput(VK_RIGHT);
+				rightStartFrames = tickFrames;												// set start timestamp
+				//SendKeyboardInput(VK_RIGHT);
 				//cout << "right pressed" << endl;
-				rightDown = true;
 			}
-			// VP 2007.02.27: this addition caused rightHeldFrames to grow with steady progression - it should be an assignment instead
-			rightHeldFrames = tickFrames - rightStartFrames;								// get however much time was spent looping, converted into WA frame lengths (0.02 seconds each)
-			bool targetRightState = (rightHeldFrames % 11 != 10);							// at the 9th frame, let go of left for a frame
-			if(rightDown != targetRightState)												// VP 2007.02.27: synchronize desired state with actual state
+
+			rightHeldFrames = tickFrames - rightStartFrames;								// get however many frames this functionality's bind has been held
+			bool targetRightState = ((long)rightHeldFrames % 11 != 10);							// at the 9th frame, let go of left for a frame
+			if(KeyIsPressed(VK_RIGHT) != targetRightState)									// VP 2007.02.27: synchronize desired state with actual state
 			{
 				SendKeyboardInput(VK_RIGHT, !targetRightState);
 				//cout << "right " << (targetRightState ? "pressed" : "released") << endl;
-				rightDown = targetRightState;
 			}
 			rightHeldLast = true;
 		}
@@ -236,20 +229,19 @@ int main()
 
 		/////////////// numpad 5 - alternate left and right arrow keys ///////////////
 
-		if(KeyIsPressed(VK_NUMPAD5) || KeyIsPressed(0x2E,true))
+		if(KeyIsPressed(VK_NUMPAD5) || KeyIsPressed(0x2D,true))
 		{
 			if(!alternateHeldLast)
 			{
 				cout << "alternating arrow keys started" << endl;
-				QueryPerformanceCounter(&alternateStart);
-				alternateStartFrames = (unsigned long long)floor((unsigned long long)alternateStart.LowPart*targetFPS/(unsigned long long)ticksPerSecond.LowPart);	// convert to WA frame lengths
+				alternateStartFrames = tickFrames;											// set start timestamp
 				SendKeyboardInput(goingRight ? VK_LEFT : VK_RIGHT);
 				//cout << (goingRight ? "left" : "right") << " pressed" << endl;
 				goingRight = !goingRight;
 			}
 
 			lastAlternateHeldFrames = alternateHeldFrames;
-			alternateHeldFrames = tickFrames - alternateStartFrames;						// get however much time was spent looping, converted into WA frame lengths (0.02 seconds each)
+			alternateHeldFrames = tickFrames - alternateStartFrames;						// get however many frames this functionality's bind has been held
 			
 			if(alternateHeldFrames > lastAlternateHeldFrames)								// only if a frame has passed
 			{
@@ -267,6 +259,8 @@ int main()
 				//cout << (goingRight ? "left released" : "right released") << endl;
 				//cout << (goingRight ? "right pressed" : "left pressed") << endl;
 			}
+			//leftHeldFrames = 0;
+			//rightHeldFrames = 0;
 			alternateHeldLast = true;
 		}
 		else if(alternateHeldLast)															// clean up if user released numpad 5
